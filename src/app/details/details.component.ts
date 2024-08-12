f377ee2196da046297b90892adef1be3f4758d02
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-08-12 13:39:04 -0400

    replaced static data with a json-server. Updated service to make HTTP calls to this server. Set up filtering

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -58,7 +58,7 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => this.housingLocation = housingLocation)
   }
 
   submitApplication() {
